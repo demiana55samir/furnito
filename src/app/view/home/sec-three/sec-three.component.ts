0760654cc6901814fea0e3198a0f1e13f3133709
demiana <demianaa55samir@gmail.com>
2024-10-28 14:45:58 +0300

    single product

--- src/app/view/home/sec-three/sec-three.component.ts
+++ src/app/view/home/sec-three/sec-three.component.ts
@@ -1,41 +1,62 @@
 import { CommonModule } from '@angular/common';
-import { Component, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 declare var $: any;
 
 @Component({
   selector: 'app-sec-three',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,CarouselModule],
   templateUrl: './sec-three.component.html',
   styleUrls: ['./sec-three.component.scss']
 })
-export class SecThreeComponent implements AfterViewInit {
+export class SecThreeComponent{
 
-  items = [
-    { image: 'path/to/image1.png', title: 'Title 1' },
-    { image: 'path/to/image2.png', title: 'Title 2' },
-    { image: 'path/to/image3.png', title: 'Title 3' },
-    { image: 'path/to/image1.png', title: 'Title 1' },
-    { image: 'path/to/image2.png', title: 'Title 2' },
-    { image: 'path/to/image3.png', title: 'Title 3' },
-    { image: 'path/to/image1.png', title: 'Title 1' },
-    { image: 'path/to/image2.png', title: 'Title 2' },
-    { image: 'path/to/image3.png', title: 'Title 3' },
-    { image: 'path/to/image1.png', title: 'Title 1' },
-    { image: 'path/to/image2.png', title: 'Title 2' },
-    { image: 'path/to/image3.png', title: 'Title 3' }
+  slides = [
+    { id:"1",image: 'image 1.png', title: 'Title 1' },
+    { id:'2',image: 'image 6.png', title: 'Title 2' },
+    { id:'3',image: 'image 3.png', title: 'Title 3' },
+    // { id:'4',image: 'image 1.png', title: 'Title 1' },
+    // { id:'5',image: 'image 2.png', title: 'Title 2' },
+    // { id:'6',image: 'image 3.png', title: 'Title 3' },
+    // { id:'7',image: 'image 1.png', title: 'Title 1' },
+    // { id:'8',image: 'image 2.png', title: 'Title 2' },
+    // { id:'9',image: 'image 3.png', title: 'Title 3' },
+    // { id:'10',image: 'image 1.png', title: 'Title 1' },
+    // { id:'11',image: 'image 2.png', title: 'Title 2' },
+    // { id:'12',image: 'image 3.png', title: 'Title 3' }
   ];
-  
-  ngAfterViewInit(): void {
 
-    $('.owl-carousel').owlCarousel({
-      items: 1,
-      loop: true,
-      margin: 10,
-      nav: true,
-      dots: true
-    });
 
+
+  ngOnInit(): void { }
+  customOptions: any = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
+    items:1,
+    
+    // navSpeed: 700,
+    // navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 2
+      }
+    },
+    // nav: true
   }
+
+ 
 }
 
--- src/app/view/shop/shop.component.ts
+++ src/app/view/shop/shop.component.ts
@@ -4,11 +4,12 @@ import { SecThreeComponent } from "../home/sec-three/sec-three.component";
 import { SecTwoComponent } from "../home/sec-two/sec-two.component";
 import { CardsComponent } from "../home/sec-two/cards/cards.component";
 import { RewardComponent } from "./reward/reward.component";
+import { DetailsComponent } from "./details/details.component";
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [ HeadshopComponent, SecThreeComponent, SecTwoComponent, CardsComponent, RewardComponent],
+  imports: [HeadshopComponent, SecThreeComponent, SecTwoComponent, CardsComponent, RewardComponent, DetailsComponent],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
 })
